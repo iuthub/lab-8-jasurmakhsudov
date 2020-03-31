ab80d7bcd03d38f821e67d5eb57e356a59a3d7b6
Jasur <j.makhsudov@mail.ru>
2020-04-01 01:54:47 +0500

    Part 2.1 finished

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
 });
+
+Route::get('/post/{id}',['as'=>'url_name', function ($id) {
+    return view('blog.post');
+}]);
